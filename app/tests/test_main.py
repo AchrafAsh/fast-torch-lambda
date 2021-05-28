025d9d771ab6be974f6fc48f63aa86d4116910c1
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-05-28 23:06:54 +0200

    :white_check_mark: test: add basic test cases for FastAPI routes

---
+++ app/tests/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_main_resource():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_child_resource():
+    response = client.get("/api/v1/test")
+    assert response.status_code == 200
