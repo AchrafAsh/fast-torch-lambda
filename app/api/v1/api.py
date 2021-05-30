8407071877a626e4a7aba142dd7c0d8e38e838c3
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-05-30 16:43:11 +0200

    :sparkles: add new endpoint to serve the model

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,18 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
-from .endpoints import services
+from models.utils import translate
+
+class Data(BaseModel):
+    sentence: str
 
 router = APIRouter()
 
-router.include_router(services.router, prefix="/test", tags=['Endpoint Test'])
+@router.get("/test")
+def testing_child_resoure():
+    return {"message": "This is a test endpoint!"}
+
+@router.post("/translate")
+def translate_sentence(data:Data):
+    translated_sentence = translate(data.sentence)
+    return { "fr": data.sentence, "en": translated_sentence }
